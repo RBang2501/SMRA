540dc54ae37b950eb3df84374330d98cfa53eff6
RBang2501 <rakshitbang0005@gmail.com>
2024-01-30 12:12:37 +0530

    Admin Dashboard Setup

---
+++ src/pages/adminDashboard.js
@@ -0,0 +1,14 @@
+// src/components/AdminDashboard.js
+import React, { useState } from 'react';
+
+const AdminDashboard = () => {
+  // Add any state or logic you need for your dashboard here
+
+  return (
+    <div>
+      Hi Admin Dashboard !
+    </div>
+  );
+};
+
+export default AdminDashboard;
