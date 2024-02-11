27bb0fc139819de5364b823c72ac9680324e8039
RBang2501 <rakshitbang0005@gmail.com>
2024-02-11 18:36:18 +0530

    Added Firebase API keys

---
+++ src/pages/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMn9RXaJc3lWzvYBSzDF3L_Hb-oC7yv_Q",
+  authDomain: "smra-499de.firebaseapp.com",
+  projectId: "smra-499de",
+  storageBucket: "smra-499de.appspot.com",
+  messagingSenderId: "324786353304",
+  appId: "1:324786353304:web:c07098ab2ce96c03c0cced"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
